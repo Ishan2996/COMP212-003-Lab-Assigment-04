f2ce1da04aed12f4042b6d82dc6bd3f73ad61889
Ishan S Sharma <Ishan S Sharma@DESKTOP-C6LS930>
2017-02-26 21:19:15 -0500

    Ex 01 Done

---
+++ Ishan_Sharma_Lab04_Ex01/CustomStackOverFlowException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishan_Sharma_Lab04_Ex01
+{
+    class CustomStackOverFlowException : Exception
+    {
+        public CustomStackOverFlowException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Ishan_Sharma_Lab04_Ex01/CustomStackUnderFlowException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishan_Sharma_Lab04_Ex01
+{
+    class CustomStackUnderFlowException : Exception
+    {
+        public CustomStackUnderFlowException(string message) : base(message)
+        {
+        }
+    }
+}
